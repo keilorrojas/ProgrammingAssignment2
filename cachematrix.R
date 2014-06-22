bc90b63066c02359f3ce1a979c76487bd7c8ba2d
Keilor Rojas <keilor.rojas@gmail.com>
2014-06-22 16:24:31 -0600

    Assignment2 matrixinverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## Assignment2 didn't have time enough to solve it 
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+m<- makeCacheMatrix()
+m$set(matrix(c(0,2,2,0),2,2))
+      m$getmean()
+      cacheSolve(m)
+      cacheSolve(m)
+      m$get()
+      m$set(matrix(c(1,0,1,0),2,2))
+      m$getmean()
+      cacheSolve(m)
+      cacheSolve(m)
+      m$get()
